e0ee7670b721db78b3ff7c52806aeb2a076a7bec
Kelvin Cluxnei <cluxnei@Kelvins-MacBook-Pro.local>
2023-11-08 19:05:47 -0300

    first commit

---
+++ exe.js
@@ -0,0 +1,183 @@
+function contaLetraMaisFrequenteClayton(palavra) {
+    let novaPalavra = palavra.split('')
+    let maior = novaPalavra[0]
+    let resposta = ''
+    let contResposta = 0
+
+    for (let i = 0; i < novaPalavra.length; i++) {
+        let contMaior = 0
+        let cont = 0
+        for (let j = 0; j < novaPalavra.length; j++) {
+            if (novaPalavra[j] == maior) {
+                contMaior++
+            }
+        }
+        if (contMaior >= cont) {
+            maior = novaPalavra[i]
+            cont = contMaior
+        }
+        contMaior = 0
+        if (cont > contResposta) {
+            contResposta = cont
+            resposta = maior
+        }
+    }
+    return resposta
+}
+
+function contaLetraMaisFrequentePedro(palavra) {
+    let letra = "A";
+    let vector = [
+        "A",
+        "B",
+        "C",
+        "D",
+        "E",
+        "F",
+        "G",
+        "H",
+        "I",
+        "J",
+        "K",
+        "L",
+        "M",
+        "N",
+        "O",
+        "P",
+        "Q",
+        "R",
+        "S",
+        "T",
+        "U",
+        "V",
+        "W",
+        "X",
+        "Y",
+        "Z",
+    ];
+    let vectorValue = new Array(26).fill(0);
+    let index = 0;
+    let i = 0;
+    let maior = 0;
+
+    while (i < palavra.length) {
+        if (palavra[i] == vector[index]) {
+            vectorValue[index]++;
+            if (maior < vectorValue[index]) {
+                maior = vectorValue[index];
+                letra = vector[index];
+            }
+            i++;
+            index = 0;
+        } else {
+            index++;
+        }
+    }
+    return letra;
+}
+
+function contaLetraMaisFrequenteKelvin(palavra) {
+    const codes = [];
+    let maxCount = 0;
+    let maxCode = 0;
+    for (let i = 0; i < palavra.length; i++) {
+        const char = palavra[i];
+        const code = char.charCodeAt(0);
+        if (codes[code]) {
+            codes[code] += 1;
+        } else {
+            codes[code] = 1;
+        }
+        if (codes[code] > maxCount) {
+            maxCount = codes[code];
+            maxCode = code;
+        }
+    }
+    return String.fromCharCode(maxCode);
+}
+
+function contaLetraMaisFrequenteLari(palavra) {
+    const letterCount = {};
+    let maxCount = 0;
+    let frequentLetter = "";
+
+    for (let letter of palavra) {
+        if (letterCount[letter]) {
+            letterCount[letter]++;
+        } else {
+            letterCount[letter] = 1;
+        }
+
+        if (letterCount[letter] > maxCount) {
+            maxCount = letterCount[letter];
+            frequentLetter = letter;
+        }
+
+    }
+
+    return frequentLetter;
+
+}
+
+
+function contaLetraMaisFrequenteLari3(palavra) {
+    const letterCount = [...palavra].reduce((map, letter) => {
+        map.set(letter, map.has(letter) ? map.get(letter) + 1 : 1);
+        return map;
+    }, new Map());
+
+    let maxCount = 0;
+    let frequentLetter = '';
+
+    letterCount.forEach((count, letter) => {
+        if (count > maxCount) {
+            maxCount = count;
+            frequentLetter = letter;
+        }
+    });
+
+    return frequentLetter;
+
+}
+
+function contaLetraMaisFrequenteLari2(palavra) {
+    const letterCount = {};
+    let maxCount = 0;
+    let frequentLetter = "";
+
+    for (let i = 0; i < palavra.length; i++) {
+        const letter = palavra[i];
+        letterCount[letter] = (letterCount[letter] || 0) + 1;
+        if (letterCount[letter] > maxCount) {
+            maxCount = letterCount[letter];
+            frequentLetter = letter;
+        }
+    }
+
+    return frequentLetter;
+
+}
+
+function contaLetraMaisFrequenteRaul(string) {
+    const characters = {};
+    let max = 0,
+        maxChar = '';
+
+    for (let i = 0; i < string.length; i++) {
+        const char = string[i];
+        characters[char] = characters[char] + 1 || 1;
+    }
+    const keys = Object.keys(characters);
+    for (let i = 0; i < keys.length; i++) {
+        const char = keys[i];
+        if (characters[char] > max) {
+            max = characters[char];
+            maxChar = char;
+        }
+    }
+
+    return maxChar;
+}
+
+
+module.exports = { contaLetraMaisFrequenteLari3, contaLetraMaisFrequenteRaul, contaLetraMaisFrequenteClayton, contaLetraMaisFrequentePedro, contaLetraMaisFrequenteLari, contaLetraMaisFrequenteLari2, contaLetraMaisFrequenteKelvin }
---
+++ runner.js
@@ -0,0 +1,164 @@
+const { contaLetraMaisFrequenteRaul, contaLetraMaisFrequenteLari3, contaLetraMaisFrequenteClayton, contaLetraMaisFrequentePedro, contaLetraMaisFrequenteLari, contaLetraMaisFrequenteLari2, contaLetraMaisFrequenteKelvin } = require('./exe.js');
+
+function shuffleWord(word) {
+    return [...word].sort(() => Math.random() - 0.5).join('')
+}
+
+function generateWord(map) {
+    let word = '';
+    for (const { char, count } of map) {
+        if (count > 1) {
+            word += char.repeat(count);
+            continue;
+        }
+        word += char;
+    }
+    return shuffleWord(word);
+}
+
+function matchOutputFor(input, expectedOutput, callback) {
+    const output = callback(input);
+    return {
+        match: output === expectedOutput,
+        output
+    };
+}
+
+function pickRandomCharFromAlphabet() {
+    const chars = 'ABCDEFGHIJKLMNOPQRSTUWXYZ';
+    return chars[Math.floor(Math.random() * chars.length)];
+}
+
+function pickRandomCharExcept(char) {
+    let random = pickRandomCharFromAlphabet();
+    while (random === char) {
+        random = pickRandomCharFromAlphabet();
+    }
+    return random;
+}
+
+function generateWordMapWithMostFrequentChar(mostFrequentChar) {
+    const mostFrequentCount = 10;
+    const map = [{
+        char: mostFrequentChar,
+        count: mostFrequentCount
+    }];
+    const distinctCharCount = 5;
+    let currentCharCount = mostFrequentCount - 2;
+    const alreadyPickedChars = new Set();
+    for (let i = 0; i < distinctCharCount; i++) {
+        let char = pickRandomCharExcept(mostFrequentChar);
+        while (alreadyPickedChars.has(char)) {
+            char = pickRandomCharExcept(mostFrequentChar);
+        }
+        if (char === mostFrequentChar) {
+            continue;
+        }
+        map.push({ char, count: currentCharCount });
+        currentCharCount -= 1;
+        alreadyPickedChars.add(char);
+    }
+    return map;
+}
+
+function getDurationInMilliseconds(start) {
+    const NS_PER_SEC = 1e9;
+    const NS_TO_MS = 1e6;
+    const diff = process.hrtime(start);
+
+    return (diff[0] * NS_PER_SEC + diff[1]) / NS_TO_MS;
+}
+
+let cachedCases = null;
+
+function getCases(cache, count) {
+    if (cache && cachedCases !== null) {
+        return cachedCases;
+    }
+    const cases = [];
+    for (let i = 0; i < count; i++) {
+        const char = pickRandomCharFromAlphabet();
+        const map = generateWordMapWithMostFrequentChar(char);
+        const word = generateWord(map);
+        cases.push({ input: word, expectedOutput: char });
+    }
+    if (cache) {
+        cachedCases = cases;
+        return cachedCases;
+    }
+    return cases;
+}
+
+function checkExe(callback, useSameCases = true, casesCount = 100, showFailsCount = 3) {
+    const cases = getCases(useSameCases, casesCount);
+    // console.log(`${casesCount} cases generated...`);
+    // console.log('matching cases...');
+    let matches = 0;
+    const startTime = process.hrtime();
+    let failsDisplayed = 0;
+    for (const { input, expectedOutput } of cases) {
+        const { match, output } = matchOutputFor(input, expectedOutput, callback);
+        if (match) {
+            matches += 1;
+            continue;
+        }
+        if (failsDisplayed < showFailsCount) {
+            console.log(`failed on case: "${input}" expected: "${expectedOutput}" but received "${output}"`);
+            failsDisplayed += 1;
+        }
+    }
+    const executionTime = getDurationInMilliseconds(startTime);
+    // console.table({
+    //     matches,
+    //     fails: casesCount - matches,
+    //     casesCount,
+    //     executionTime: executionTime.toFixed(2) + 'ms',
+    //     callbackName: callback.name,
+    // })
+
+    return executionTime
+}
+
+// checkExe(contaLetraMaisFrequenteClayton, true, 100, 2);
+// checkExe(contaLetraMaisFrequentePedro, true, 100, 2);
+// checkExe(contaLetraMaisFrequenteLari, false, 100, 2);
+// checkExe(contaLetraMaisFrequenteLari, false, 1000, 2);
+// checkExe(contaLetraMaisFrequenteLari, false, 10000, 2);
+// checkExe(contaLetraMaisFrequenteLari, false, 100000, 2);
+
+// checkExe(contaLetraMaisFrequenteKelvin, false, 100, 2);
+// checkExe(contaLetraMaisFrequenteKelvin, false, 1000, 2);
+// checkExe(contaLetraMaisFrequenteKelvin, false, 10000, 2);
+let times = [
+    checkExe(contaLetraMaisFrequenteLari, false, 100000, 2),
+    checkExe(contaLetraMaisFrequenteLari, false, 100000, 2),
+    checkExe(contaLetraMaisFrequenteLari, false, 100000, 2),
+    checkExe(contaLetraMaisFrequenteLari, false, 100000, 2),
+    checkExe(contaLetraMaisFrequenteLari, false, 100000, 2),
+    checkExe(contaLetraMaisFrequenteLari, false, 100000, 2),
+];
+
+console.log('avg lari v1: ' + (times.reduce((acc, time) => acc + time, 0) / times.length).toFixed(2) + 'ms');
+
+times = [
+    checkExe(contaLetraMaisFrequenteLari3, false, 100000, 2),
+    checkExe(contaLetraMaisFrequenteLari3, false, 100000, 2),
+    checkExe(contaLetraMaisFrequenteLari3, false, 100000, 2),
+    checkExe(contaLetraMaisFrequenteLari3, false, 100000, 2),
+    checkExe(contaLetraMaisFrequenteLari3, false, 100000, 2),
+    checkExe(contaLetraMaisFrequenteLari3, false, 100000, 2),
+];
+
+console.log('avg lari v3: ' + (times.reduce((acc, time) => acc + time, 0) / times.length).toFixed(2) + 'ms');
+
+
+times = [
+    checkExe(contaLetraMaisFrequenteKelvin, false, 100000, 2),
+    checkExe(contaLetraMaisFrequenteKelvin, false, 100000, 2),
+    checkExe(contaLetraMaisFrequenteKelvin, false, 100000, 2),
+    checkExe(contaLetraMaisFrequenteKelvin, false, 100000, 2),
+    checkExe(contaLetraMaisFrequenteKelvin, false, 100000, 2),
+    checkExe(contaLetraMaisFrequenteKelvin, false, 100000, 2),
+];
+
+console.log('avg kelvin v1: ' + (times.reduce((acc, time) => acc + time, 0) / times.length).toFixed(2) + 'ms');
